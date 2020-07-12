2b68be5098e504a196bdeb302b935b0d6781e630
Robert Swarthout <robert@shootproof.com>
2020-07-12 19:13:41 -0400

    Setting environment variables instance wide.

--- .aws/code-deploy/after-install.sh
+++ .aws/code-deploy/after-install.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+AWS_REGION="`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`"
+
+echo "AWS_REGION=${AWS_REGION}" >> /etc/environment
+echo "PAYID_ENVIRONMENT=production" >> /etc/environment
+
+echo "export AWS_REGION=\"${AWS_REGION}\"" >> /etc/sysconfig/httpd
+echo "export PAYID_ENVIRONMENT=\"production\"" >> /etc/sysconfig/httpd
